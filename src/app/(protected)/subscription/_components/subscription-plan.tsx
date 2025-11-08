2c025160ec92af25db5e64ff997bd6d872f118fc
Gabriel Massaia <gabrielmassaia50@gmail.com>
2025-11-08 06:51:35 -0300

    Feat: Implementa tratativa pra null values

--- src/app/(protected)/subscription/_components/subscription-plan.tsx
+++ src/app/(protected)/subscription/_components/subscription-plan.tsx
@@ -62,7 +62,7 @@ export function SubscriptionPlan({
   };
 
   const priceLabel =
-    typeof plan.priceInCents === "number"
+    typeof plan?.priceInCents === "number"
       ? new Intl.NumberFormat("pt-BR", {
           style: "currency",
           currency: "BRL",
@@ -73,17 +73,17 @@ export function SubscriptionPlan({
     <Card className={className}>
       <CardHeader>
         <div className="flex items-center justify-between">
-          <h3 className="text-2xl font-bold text-gray-900">{plan.name}</h3>
+          <h3 className="text-2xl font-bold text-gray-900">{plan?.name}</h3>
           {isActive && (
             <Badge className="bg-green-100 text-green-700 hover:bg-green-100">
               Atual
             </Badge>
           )}
         </div>
-        <p className="text-gray-600">{plan.description}</p>
+        <p className="text-gray-600">{plan?.description}</p>
         <div className="flex items-baseline">
           <span className="text-3xl font-bold text-gray-900">{priceLabel}</span>
-          {typeof plan.priceInCents === "number" && (
+          {typeof plan?.priceInCents === "number" && (
             <span className="ml-1 text-gray-600">/ mês</span>
           )}
         </div>
@@ -91,7 +91,7 @@ export function SubscriptionPlan({
 
       <CardContent>
         <div className="space-y-4 border-t border-gray-200 pt-6">
-          {plan.features.map((feature, index) => (
+          {plan?.features.map((feature, index) => (
             <div key={index} className="flex items-start">
               <div className="flex-shrink-0">
                 <CheckCircle2 className="h-5 w-5 text-green-500" />
@@ -105,19 +105,17 @@ export function SubscriptionPlan({
           <Button
             className="w-full"
             variant="outline"
-            onClick={
-              isActive ? handleManagePlanClick : handleSubscribeClick
-            }
+            onClick={isActive ? handleManagePlanClick : handleSubscribeClick}
             disabled={
               createStripeCheckoutAction.isExecuting ||
-              plan.comingSoon ||
+              plan?.comingSoon ||
               (isActive && plan.slug === "free") ||
-              (!plan.stripePriceId && !isActive)
+              (!plan?.stripePriceId && !isActive)
             }
           >
             {createStripeCheckoutAction.isExecuting ? (
               <Loader2 className="mr-1 h-4 w-4 animate-spin" />
-            ) : plan.comingSoon ? (
+            ) : plan?.comingSoon ? (
               "Em breve"
             ) : isActive ? (
               "Gerenciar assinatura"
